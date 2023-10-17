933638b5959acac0fdcbe5ce71b9d3387e4fb4a4
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-10-17 10:00:58 -0300

    Alteração para descrever tipo de triângiulo. Última alteração.

--- Projeto 1.py
+++ Projeto 1.py
@@ -7,14 +7,13 @@ a = int(input('Informe o lado a: '))
 b = int(input('Informe o lado b: '))
 c = int(input('informe o lado c: '))
 
-if a < (b+c) and b < (a+c) and c < (a+b):
+if a <= (b+c) and b <= (a+c) and c <= (a+b):
     print('Forma um triângulo.')
-else:
-    print('Não forma um triângulo.')
-
     if (a == b) and (b == c) and (c == a):
         print('E esse triângulo é EQUILÁTERO.')
     elif (a == b) or (b == c) or (c == a):
         print('E esse triângulo é ISÓSCELES.')
     else:
-        print('E esse triângulo é .ESCALENO')
+        print('E esse triângulo é ESCALENO.')
+else:
+    print('Não forma um triângulo.')
