57cf1da83b78b641a272065eed9b8d7a7e040aa5
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-10-17 09:29:26 -0300

    Alteração para descrever tipo de triângiulo.

--- Projeto 1.py
+++ Projeto 1.py
@@ -1,11 +1,20 @@
 #Validador de triângulo
+
 print('\n\t\t\t -- Validador de triângulo --')
+
 #Entradas
 a = int(input('Informe o lado a: '))
 b = int(input('Informe o lado b: '))
 c = int(input('informe o lado c: '))
 
 if a < (b+c) and b < (a+c) and c < (a+b):
-    print('Forma um triângulo')
+    print('Forma um triângulo.')
 else:
-    print('Não forma um triângulo')
+    print('Não forma um triângulo.')
+
+if (a == b) and (b == c) and (c == a):
+    print('E esse triângulo é EQUILÁTERO.')
+elif (a == b) or (b == c) or (c == a):
+    print('E esse triângulo é ISÓSCELES.')
+else:
+    print('E esse triângulo é .ESCALENO')
